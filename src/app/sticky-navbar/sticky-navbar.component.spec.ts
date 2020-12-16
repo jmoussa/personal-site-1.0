91eee74be6b5d7dd8b0516a1ff365990362fb2e5
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2020-12-16 18:35:58 -0500

    updated angular core version

--- src/app/sticky-navbar/sticky-navbar.component.spec.ts
+++ src/app/sticky-navbar/sticky-navbar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StickyNavbarComponent } from './sticky-navbar.component';
 
@@ -6,7 +6,7 @@ describe('StickyNavbarComponent', () => {
   let component: StickyNavbarComponent;
   let fixture: ComponentFixture<StickyNavbarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StickyNavbarComponent ]
     })
