1803dbc28e9e86469d683ab192eca75fadc9a4a8
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2021-01-18 20:15:59 -0500

    update projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,142 +1,195 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {trigger,
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  trigger,
   state,
   style,
   animate,
   transition,
   query,
-  stagger
-} from '@angular/animations';
-import * as $ from 'jquery';
+  stagger,
+} from "@angular/animations";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss'],
+  selector: "app-projects",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.scss"],
   encapsulation: ViewEncapsulation.Emulated,
   animations: [
-    trigger('slideUp', [
-      state('showDetails', style({
-        paddingTop: '0',
-      })),
-      transition('* => showDetails', [
-        animate('500ms ease')
-      ])
+    trigger("slideUp", [
+      state(
+        "showDetails",
+        style({
+          paddingTop: "0",
+        })
+      ),
+      transition("* => showDetails", [animate("500ms ease")]),
     ]),
-    trigger('slide', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(-200%)'}),
-        animate('500ms ease-out', style({opacity: '1', transform: 'translateX(0)'})),
+    trigger("slide", [
+      transition(":enter", [
+        style({ opacity: 0, transform: "translateX(-200%)" }),
+        animate(
+          "500ms ease-out",
+          style({ opacity: "1", transform: "translateX(0)" })
+        ),
       ]),
-      transition(':leave', [
-        animate('500ms ease-out', style({opacity: '0',  transform: 'translateX(200%)'}))
-      ])
-    ]),
-    trigger('slideOpposite', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(200%)'}),
-        animate('500ms ease-out', style({opacity: '1', transform: 'translateX(0)'})),
+      transition(":leave", [
+        animate(
+          "500ms ease-out",
+          style({ opacity: "0", transform: "translateX(200%)" })
+        ),
       ]),
-      transition(':leave', [
-        animate('500ms ease-out', style({opacity: '0',  transform: 'translateX(-200%)'}))
-      ])
     ]),
-    trigger('staggerGrow', [
-      transition(':enter', [
-        query('.cards', [
-          style({opacity: '0', transform: 'scale(0)'}),
-          stagger('100ms', [
-            animate('750ms ease-out', style({opacity: '1', transform: 'scale(1)'})),
+    trigger("slideOpposite", [
+      transition(":enter", [
+        style({ opacity: 0, transform: "translateX(200%)" }),
+        animate(
+          "500ms ease-out",
+          style({ opacity: "1", transform: "translateX(0)" })
+        ),
+      ]),
+      transition(":leave", [
+        animate(
+          "500ms ease-out",
+          style({ opacity: "0", transform: "translateX(-200%)" })
+        ),
+      ]),
+    ]),
+    trigger("staggerGrow", [
+      transition(":enter", [
+        query(".cards", [
+          style({ opacity: "0", transform: "scale(0)" }),
+          stagger("100ms", [
+            animate(
+              "750ms ease-out",
+              style({ opacity: "1", transform: "scale(1)" })
+            ),
           ]),
-        ])
-      ])
+        ]),
+      ]),
     ]),
   ],
 })
 export class ProjectsComponent implements OnInit {
-  public val = '';
+  public val = "";
 
   stocks: boolean;
   chat_app: boolean;
   d3: boolean;
+  luxor: boolean;
+  spotify: boolean;
   selected: boolean;
   inView: boolean;
   projectView: boolean;
 
   navigateToGithub(s: String) {
     // console.log( s );
-    window.open('https://github.com/jmoussa/' + s, '_blank');
+    window.open("https://github.com/jmoussa/" + s, "_blank");
   }
   navigateToWebsite() {
-    window.open('http://www.expensereport.link/')
+    window.open("http://www.expensereport.link/");
   }
 
-  constructor() { }
-  isInViewport (el) {
+  constructor() {}
+  isInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+      rect.bottom <=
+        (window.innerHeight ||
+          document.documentElement.clientHeight) /*or $(window).height() */ &&
+      rect.right <=
+        (window.innerWidth ||
+          document.documentElement.clientWidth) /*or $(window).width() */
     );
   }
   isInView(el, callback) {
     let old_visible = false;
-    return ( () => {
+    return () => {
       const visible: boolean = this.isInViewport(el);
       if (visible !== old_visible) {
         old_visible = visible;
-        if (typeof callback === 'function') {
+        if (typeof callback === "function") {
           callback();
         }
       }
-    });
+    };
   }
   ngOnInit() {
     this.stocks = false;
-    this.chat_app= false;
+    this.chat_app = false;
     this.d3 = false;
     this.selected = false;
     this.inView = false;
     this.projectView = false;
 
-    const handler = this.isInView(document.querySelector('.project-header'), () => {
-      this.inView = true;
-    });
-    const projectHandler = this.isInView(document.querySelector('.project-container'), () => {
-      this.projectView = true;
-    });
+    const handler = this.isInView(
+      document.querySelector(".project-header"),
+      () => {
+        this.inView = true;
+      }
+    );
+    const projectHandler = this.isInView(
+      document.querySelector(".project-container"),
+      () => {
+        this.projectView = true;
+      }
+    );
 
-    $(window).on('DOMContentLoaded load resize scroll', projectHandler);
-    $(window).on('DOMContentLoaded load resize scroll', handler);
+    $(window).on("DOMContentLoaded load resize scroll", projectHandler);
+    $(window).on("DOMContentLoaded load resize scroll", handler);
   }
 
   showDetails(choice: String) {
     this.selected = true;
     switch (choice) {
-      case 'stocks': {
+      case "stocks": {
         this.stocks = true;
-        this.chat_app= false;
+        this.chat_app = false;
         this.d3 = false;
+        this.luxor = false;
+        this.spotify = false;
         break;
       }
-      case 'chat_app': {
+      case "chat_app": {
         this.stocks = false;
-        this.chat_app= true;
+        this.chat_app = true;
         this.d3 = false;
+        this.luxor = false;
+        this.spotify = false;
         break;
       }
-      case 'd3': {
+      case "d3": {
         this.stocks = false;
-        this.chat_app= false;
+        this.chat_app = false;
         this.d3 = true;
+        this.luxor = false;
+        this.spotify = false;
+        break;
+      }
+      case "luxor": {
+        this.stocks = false;
+        this.chat_app = false;
+        this.d3 = false;
+        this.luxor = true;
+        this.spotify = false;
+        break;
+      }
+      case "spotify": {
+        this.stocks = false;
+        this.chat_app = false;
+        this.d3 = false;
+        this.luxor = false;
+        this.spotify = true;
         break;
       }
       default: {
         this.stocks = false;
-        this.chat_app= false;
+        this.chat_app = false;
         this.d3 = false;
+        this.luxor = false;
+        this.spotify = false;
         break;
       }
     }
